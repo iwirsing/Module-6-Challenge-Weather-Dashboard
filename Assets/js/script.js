175460607b19ecd77802c930b83e98766530a0aa
iwirsing <ivymolina@gmail.com>
2022-11-03 01:16:21 -0500

    comment JS file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -15,16 +15,12 @@ function searchWeather(event){
     if (cityArray.includes(cityName)){
         alert("The city is already saved. Click "+cityName+" button to check the weather");
     }
-    
     else{
     fromSaved=false;
     fetchCoordinates(cityName,fromSaved);
     }
-
-   
 }
-
-//function fetch coordinates
+// FUNCTION 1: get the coordinates of the cities
 function fetchCoordinates(cityName,isItSaved){
      //get coordinates of place
      var coordinatesURL="http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=1&appid=c8a02479b2ade74cf8b10337c300e1d5";
@@ -46,11 +42,9 @@ function fetchCoordinates(cityName,isItSaved){
      .catch(function (error) {
        alert('Unable to connect to GitHub');
      });
-
 }
 
-
-//get the Weather
+//FUNCTION 2: get weather using the coordinates, isItSaved is boolean for whether the user clicked on search or saved cities
 function getWeather(lat, lon, isItSaved){
     console.log("latitude: "+lat);
     console.log("longitude: "+lon);
@@ -77,22 +71,24 @@ function getWeather(lat, lon, isItSaved){
             for (var j=1;j<6;j++){
                 var dayID="#Day"+(j);
                 var i=(9+(j*6));
-
+                
+                //set date
                 var daysAfter=momentToday;
                 daysAfter.setDate(daysAfter.getDate()+j);
                 
-
+                //display date
                 displayForecast(daysAfter.toLocaleDateString(),weather.list[i].weather[0].main,weather.list[i].main.temp,weather.list[i].wind.speed,weather.list[i].main.humidity,dayID,weather.list[i].weather[0].icon);
                 console.log(weather.list[i].dt_txt.substring(0,10));
+                
+                //reset date 
                 momentToday=new Date(today*1000);
 
             }
-
+            //if the search is not from the saved cities button, create new city icon
             if (!isItSaved){
             //create button to save data on city name
             saveCity(city);
             }
-
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -101,11 +97,9 @@ function getWeather(lat, lon, isItSaved){
     .catch(function (error) {
       alert('Unable to connect to GitHub');
     });
-
 }
 
-//display Today
-
+//FUNCTION 3: display Today's weather
 function displayToday(city,date,weather,temp, wind,humidity,icon){
     console.log(city,date,weather,temp, wind,humidity, icon)
 
@@ -117,7 +111,7 @@ function displayToday(city,date,weather,temp, wind,humidity,icon){
     var getIconURL="http://openweathermap.org/img/wn/"+icon+"@2x.png"
     weatherIcon.setAttribute("src",getIconURL);
 
-    //check weather and set background color
+    //check weather and set background color for the element to reflect weather
     if(weather==="Clouds"){
         todayWeatherEl.setAttribute("style","background-color:#c5c5c5");
     }
@@ -130,9 +124,6 @@ function displayToday(city,date,weather,temp, wind,humidity,icon){
     if(weather==="Snow"){
         todayWeatherEl.setAttribute("style","background-color:white");
     }
-
-
-
     //convert Kelvin to F
     var tempF=Math.round(1.8*(Number(temp)-273)+32);
     console.log(tempF);
@@ -141,15 +132,16 @@ function displayToday(city,date,weather,temp, wind,humidity,icon){
     var speedMPH=Math.round(2.237*wind);
     console.log(speedMPH);
 
-    //display
+    //display today's weather
     var todayHeader=document.createElement("h3");
     var headerContent=document.createTextNode(city+" ("+date+") ");
     todayHeader.appendChild(headerContent);
     console.log(todayHeader);
-
+    //append element to DOM
     todayWeatherEl.appendChild(todayHeader);
     todayWeatherEl.appendChild(weatherIcon);
 
+    //populate weather parameters in a list
     var weatherList=document.createElement("ul");
     weatherList.setAttribute("style","list-style-type: none; margin: 0; padding:0");
     //temperature
@@ -162,16 +154,17 @@ function displayToday(city,date,weather,temp, wind,humidity,icon){
     var humidData=document.createElement("li");
     humidData.textContent="Humidity: "+humidity+"%";
 
-    //append elements
+    //append list elements to DOM
     weatherList.appendChild(temperature);
     weatherList.appendChild(windSpeed);
     weatherList.appendChild(humidData);
     todayWeatherEl.appendChild(weatherList);
 }
 
-//display weather
+//FUNCTION 4: display the next five days of weather
 function displayForecast(date,weather,temp, wind,humidity,id,icon){
     console.log(id);
+
     //clear container
     document.querySelector(id).textContent="";
 
@@ -193,49 +186,47 @@ function displayForecast(date,weather,temp, wind,humidity,id,icon){
         document.querySelector(id).setAttribute("style","background-color:#8989ff");
     }
     if(weather==="Snow"){
-
         document.querySelector(id).setAttribute("style","background-color:white");
     }
     
     //convert Kelvin to F
     var tempF=Math.round(1.8*(Number(temp)-273)+32);
-    console.log(tempF);
 
-    //convert speed to ft/second
-    var speedFt=Math.round(3.280*wind);
-    console.log(speedFt);
-
-      //display
-      
-      var forecastContent=document.createTextNode(date);
-      forecastHeader.appendChild(forecastContent);
+    //convert speed to MPH
+    var speedMPH=Math.round(3.280*wind);
     
+    //display
+    var forecastContent=document.createTextNode(date);
+    forecastHeader.appendChild(forecastContent);
+    //append to DOM
+    document.querySelector(id).appendChild(forecastHeader);
+    document.querySelector(id).appendChild(weatherIcon);
+
+    //populate weather parameters
+    weatherList.setAttribute("style","list-style-type: none; margin: 0; padding:0");
+    //temperature
+    var temperature=document.createElement("li");
+    temperature.textContent="Temp: "+tempF+" F";
+    //wind
+    var windSpeed=document.createElement("li");
+    windSpeed.textContent="Wind: "+speedMPH+" MPH";
+    //humidity
+    var humidData=document.createElement("li");
+    humidData.textContent="Humidity: "+humidity+"%";
   
-      document.querySelector(id).appendChild(forecastHeader);
-      document.querySelector(id).appendChild(weatherIcon);
-  
-      weatherList.setAttribute("style","list-style-type: none; margin: 0; padding:0");
-      //temperature
-      var temperature=document.createElement("li");
-      temperature.textContent="Temp: "+tempF+" F";
-      //wind
-      var windSpeed=document.createElement("li");
-      windSpeed.textContent="Wind: "+speedFt+" ft/s";
-      //humidity
-      var humidData=document.createElement("li");
-      humidData.textContent="Humidity: "+humidity+"%";
-  
-      //append elements
-      weatherList.appendChild(temperature);
-      weatherList.appendChild(windSpeed);
-      weatherList.appendChild(humidData);
-      document.querySelector(id).appendChild(weatherList);
+    //append elements
+    weatherList.appendChild(temperature);
+    weatherList.appendChild(windSpeed);
+    weatherList.appendChild(humidData);
+    document.querySelector(id).appendChild(weatherList);
 }
 
-//save weather to local Storage
+//FUNCTION 5: save city to local Storage
 function saveCity(city){
+    //display the city button
     displayCityButtons (city);
-    //display delete button
+
+    //display delete button now that there is city button/s
     document.querySelector("#delBtn").setAttribute("style","display:visible");
 
     //save data into local Storage
@@ -243,71 +234,61 @@ function saveCity(city){
     localStorage.setItem("savedCities",JSON.stringify(cityArray));
 }
 
-//display saved cities
+//FUNCTION 6: display saved cities
 function displayCityButtons(city){
-     //create button
-     var blockContainer=document.createElement("div");
-     var cityData= document.createElement("button");
-     cityData.setAttribute("class","btn btn-secondary cityBtn");
-     cityData.setAttribute("type","button");
-     cityData.setAttribute("id",city);
-     cityData.setAttribute("style","margin-top:1rem;font-size:0.75rem");
-     cityData.textContent=city;
-     blockContainer.appendChild(cityData);
-     //append button
-     document.querySelector("#savedCities").appendChild(blockContainer);
-
+    //create city button
+    var blockContainer=document.createElement("div");
+    var cityData= document.createElement("button");
+    cityData.setAttribute("class","btn btn-secondary cityBtn");
+    cityData.setAttribute("type","button");
+    cityData.setAttribute("id",city);
+    cityData.setAttribute("style","margin-top:1rem;font-size:0.75rem");
+    cityData.textContent=city;
+    blockContainer.appendChild(cityData);
+    //append button
+    document.querySelector("#savedCities").appendChild(blockContainer);
 }
 
-//initiate page
+//FUNCTION 7: initialize page
 function initializePage(){
-   
     //check if there is any in the storage
    if(JSON.parse(localStorage.getItem("savedCities"))!=null){
 
-     //get file from local storage
-     var getCities=JSON.parse(localStorage.getItem("savedCities"));
+    //get file from local storage
+    var getCities=JSON.parse(localStorage.getItem("savedCities"));
 
     //display
     for (var i=0;i<getCities.length;i++){
-        displayCityButtons(getCities[i]);}
-    // }
-
+        displayCityButtons(getCities[i]);
+    }
+    //set stored data to city array
     cityArray=getCities;
-    
-     //display delete button
-     document.querySelector("#delBtn").setAttribute("style","display:visible");
-   }
 
+    //display delete button
+    document.querySelector("#delBtn").setAttribute("style","display:visible");
+    }
 }
 
-
-
-
+//initialize
 initializePage();
 
 //search button
 searchBtnEl.on("click", searchWeather);
 
-
-
-//delete cities button
+//delete cities button click
 $("#delBtn").on("click",function(event){
     event.preventDefault();
     console.log("clicked");
     //delete local storage
     localStorage.removeItem("savedCities");
-
     //hide delete button
     document.querySelector("#delBtn").setAttribute("style","display:none");
-
+    //delete the cities
     document.querySelector("#savedCities").innerHTML="";
     initializePage();
-
-
 });
 
-//city button
+//saved city buttons click
 $(".cityBtn").on("click",function(event)
 {
     event.preventDefault();
